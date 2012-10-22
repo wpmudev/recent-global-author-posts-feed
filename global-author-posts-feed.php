9f9e2d1ba59dfe03fd442850e209a40b5b09f39d
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2012-10-22 11:11:34 +0000

    Updated for new version of postindexer Now uses a permalink URL for the feed rather than having to be placed in wp-content Included an update of old version in case needed
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/recent-global-author-posts-feed@7520 f53fe46e-8c02-459b-9fdd-93e75771b278

---
+++ global-author-posts-feed.php
@@ -0,0 +1,165 @@
+<?php
+/*
+Plugin Name: Recent Global Author Posts Feed
+Plugin URI:
+Description: RSS2 feeds
+Version: 3.0
+Author: Barry (Incsub)
+Author URI:
+WDP ID:
+Network: true
+*/
+
+/*
+Copyright 2012 Incsub (http://incsub.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License (Version 2 - GPLv2) as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+
+class globalauthorpostsfeed {
+
+	var $build = 1;
+
+	var $db;
+
+	function __construct() {
+
+		global $wpdb;
+
+		$this->db =& $wpdb;
+
+		if($this->db->blogid == 1) {
+			// Only add the feed for the main site
+			add_action('init', array(&$this, 'initialise_global_author_posts_feed') );
+		}
+
+	}
+
+	function globalauthorpostsfeed() {
+		$this->__construct();
+	}
+
+	function initialise_global_author_posts_feed() {
+
+		global $wp_rewrite;
+
+		$installed = get_option('globalauthorpostsfeed_version', false);
+
+		add_feed('globalauthorpostsfeed', array( &$this, 'do_global_author_posts_feed' ));
+
+		if($installed === false || $installed < $this->build) {
+			// We need to flush our rewrites so that the new feed is added and recognised
+			flush_rewrite_rules();
+			update_option('globalauthorpostsfeed_version', $this->build);
+		}
+
+	}
+
+	function do_global_author_posts_feed() {
+
+		global $network_query, $network_post;
+
+		// Remove all excerpt more filters
+		remove_all_filters('excerpt_more');
+
+		@header('Content-Type: ' . feed_content_type('rss-http') . '; charset=' . get_option('blog_charset'), true);
+		$more = 1;
+
+		echo '<?xml version="1.0" encoding="' . get_option('blog_charset') . '"?' . '>';
+
+		$number = $_GET['number'];
+		if ( empty( $number ) ) {
+			$number = '25';
+		}
+
+		$author = $_GET['author'];
+		if ( empty( $author ) ) {
+			$author = 1;
+		}
+
+		if(!is_numeric($author)) {
+			// We could have the user login of the user so find the id from that
+			$theauthor = get_user_by( 'login', $author );
+			if(is_object($theauthor)) {
+				$author = $theauthor->ID;
+			}
+		} else {
+			$theauthor = get_user_by( 'id', $author );
+		}
+
+		$posttype = $_GET['posttype'];
+		if(empty($posttype)) {
+			$posttype = 'post';
+		}
+
+		$network_query = network_query_posts( array( 'post_type' => $posttype, 'posts_per_page' => $number, 'author' => $author ));
+
+		?>
+		<rss version="2.0"
+			xmlns:content="http://purl.org/rss/1.0/modules/content/"
+			xmlns:wfw="http://wellformedweb.org/CommentAPI/"
+			xmlns:dc="http://purl.org/dc/elements/1.1/"
+			xmlns:atom="http://www.w3.org/2005/Atom"
+			xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+			xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+			<?php do_action('rss2_ns'); ?>
+		>
+
+		<channel>
+			<title><?php bloginfo_rss('name'); _e(' - Recent Global Posts For Author : ','postindexer'); echo $theauthor->display_name; ?></title>
+			<atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml" />
+			<link><?php bloginfo_rss('url') ?></link>
+			<description><?php bloginfo_rss("description") ?></description>
+			<lastBuildDate><?php echo mysql2date('D, d M Y H:i:s +0000', network_get_lastpostmodified('GMT'), false); ?></lastBuildDate>
+			<language><?php bloginfo_rss( 'language' ); ?></language>
+			<sy:updatePeriod><?php echo apply_filters( 'rss_update_period', 'hourly' ); ?></sy:updatePeriod>
+			<sy:updateFrequency><?php echo apply_filters( 'rss_update_frequency', '1' ); ?></sy:updateFrequency>
+			<?php do_action('rss2_head'); ?>
+			<?php while( network_have_posts()) : network_the_post(); ?>
+			<item>
+				<title><?php network_the_title_rss(); ?></title>
+				<link><?php network_the_permalink_rss(); ?></link>
+				<comments><?php network_comments_link_feed(); ?></comments>
+				<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', network_get_post_time('Y-m-d H:i:s', true), false); ?></pubDate>
+				<dc:creator><?php network_the_author(); ?></dc:creator>
+				<?php network_the_category_rss('rss2'); ?>
+
+				<guid isPermaLink="false"><?php network_the_guid(); ?></guid>
+		<?php if (get_option('rss_use_excerpt')) { ?>
+				<description><![CDATA[<?php network_the_excerpt_rss(); ?>]]></description>
+		<?php } else { ?>
+				<description><![CDATA[<?php network_the_excerpt_rss() ?>]]></description>
+			<?php if ( strlen( $network_post->post_content ) > 0 ) { ?>
+				<content:encoded><![CDATA[<?php network_the_content_feed('rss2'); ?>]]></content:encoded>
+			<?php } else { ?>
+				<content:encoded><![CDATA[<?php network_the_excerpt_rss(); ?>]]></content:encoded>
+			<?php } ?>
+		<?php } ?>
+				<wfw:commentRss><?php echo esc_url( network_get_post_comments_feed_link(null, 'rss2') ); ?></wfw:commentRss>
+				<slash:comments><?php echo network_get_comments_number(); ?></slash:comments>
+		<?php network_rss_enclosure(); ?>
+			<?php do_action('network_rss2_item'); ?>
+			</item>
+			<?php endwhile; ?>
+		</channel>
+		</rss>
+		<?php
+	}
+
+}
+
+$globalauthorpostsfeed = new globalauthorpostsfeed();
+
+?>
--- recent-global-author-posts-feed.php
+++ recent-global-author-posts-feed.php
@@ -1,12 +1,6 @@
 <?php
 /*
-Plugin Name: Recent Global Author Posts Feed
-Plugin URI:
-Description: RSS2 feeds
-Author: Andrew Billits (Incsub) / Barry (Incsub)
-Version: 2.3
-Author URI:
-WDP ID: 87
+* The original method of getting the tag feed but using the new code - this should be replaced with the new plugin now
 */
 
 /*
@@ -31,20 +25,16 @@ if (empty($wp)) {
 	wp('feed=rss2');
 }
 
-/* -------------------- Update Notifications Notice -------------------- */
-if ( !function_exists( 'wdp_un_check' ) ) {
-  add_action( 'admin_notices', 'wdp_un_check', 5 );
-  add_action( 'network_admin_notices', 'wdp_un_check', 5 );
-  function wdp_un_check() {
-    if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'edit_users' ) )
-      echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
-  }
-}
-/* --------------------------------------------------------------------- */
+global $network_query, $network_post;
 
-//------------------------------------------------------------------------//
-//---Config---------------------------------------------------------------//
-//------------------------------------------------------------------------//
+// Remove all excerpt more filters
+remove_all_filters('excerpt_more');
+
+@header('Content-Type: ' . feed_content_type('rss-http') . '; charset=' . get_option('blog_charset'), true);
+$more = 1;
+
+echo '<?xml version="1.0" encoding="' . get_option('blog_charset') . '"?' . '>';
+
 $number = $_GET['number'];
 if ( empty( $number ) ) {
 	$number = '25';
@@ -52,7 +42,17 @@ if ( empty( $number ) ) {
 
 $author = $_GET['author'];
 if ( empty( $author ) ) {
-	$author = '0';
+	$author = 1;
+}
+
+if(!is_numeric($author)) {
+	// We could have the user login of the user so find the id from that
+	$theauthor = get_user_by( 'login', $author );
+	if(is_object($theauthor)) {
+		$author = $theauthor->ID;
+	}
+} else {
+	$theauthor = get_user_by( 'id', $author );
 }
 
 $posttype = $_GET['posttype'];
@@ -60,69 +60,54 @@ if(empty($posttype)) {
 	$posttype = 'post';
 }
 
-//------------------------------------------------------------------------//
-//---Functions------------------------------------------------------------//
-//------------------------------------------------------------------------//
-global $wpdb;
-
-$query = $wpdb->prepare("SELECT * FROM " . $wpdb->base_prefix . "site_posts WHERE site_id = %d AND post_author = %s AND blog_public = '1' AND post_type = %s ORDER BY post_published_gmt DESC LIMIT %d", $current_site->id, $author, $posttype, $number);
-$posts = $wpdb->get_results( $query, ARRAY_A );
-
-if ( count( $posts ) > 0 ) {
-	$last_published_post_date_time = $wpdb->get_var( $wpdb->prepare("SELECT post_published_gmt FROM " . $wpdb->base_prefix . "site_posts WHERE site_id = %d AND post_author = %s AND blog_public = '1' AND post_type = %s ORDER BY post_published_gmt DESC LIMIT 1", $current_site->id, $author, $posttype ) );
-} else {
-	$last_published_post_date_time = time();
-}
-
-if ( $author > 0 ) {
-	$author_user_login = $wpdb->get_var( $wpdb->prepare("SELECT user_login FROM " . $wpdb->base_prefix . "users WHERE ID = %s", $author ) );
-}
-
-header('Content-Type: text/xml; charset=' . get_option('blog_charset'), true);
-$more = 1;
+$network_query = network_query_posts( array( 'post_type' => $posttype, 'posts_per_page' => $number, 'author' => $author ));
 
 ?>
-<?php echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
 <rss version="2.0"
 	xmlns:content="http://purl.org/rss/1.0/modules/content/"
 	xmlns:wfw="http://wellformedweb.org/CommentAPI/"
 	xmlns:dc="http://purl.org/dc/elements/1.1/"
 	xmlns:atom="http://www.w3.org/2005/Atom"
 	xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+	xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+	<?php do_action('rss2_ns'); ?>
 >
 
 <channel>
-	<title><?php bloginfo_rss('name'); ?> <?php echo $author_user_login . ' '; ?><?php _e('Posts'); ?></title>
+	<title><?php bloginfo_rss('name'); _e(' - Recent Global Posts For Author : ','postindexer'); echo $theauthor->display_name; ?></title>
 	<atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml" />
 	<link><?php bloginfo_rss('url') ?></link>
 	<description><?php bloginfo_rss("description") ?></description>
-	<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', $last_published_post_date_time, false); ?></pubDate>
-	<?php the_generator( 'rss2' ); ?>
-	<language><?php echo get_option('rss_language'); ?></language>
-    <?php
-	//--------------------------------------------------------------------//
-	if ( count( $posts ) > 0 ) {
-		foreach ($posts as $post) {
-			$author_display_name = $wpdb->get_var( $wpdb->prepare( "SELECT display_name FROM " . $wpdb->base_prefix . "users WHERE ID = %d", $post['post_author']) );
-			?>
-			<item>
-				<title><?php echo apply_filters( 'the_title_rss', $post['post_title']); ?></title>
-				<link><?php echo $post['post_permalink']; ?></link>
-				<comments><?php echo $post['post_permalink'] . '#comments'; ?></comments>
-				<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', $post['post_published_gmt'], false); ?></pubDate>
-				<dc:creator><?php echo $author_display_name; ?></dc:creator>
-				<guid isPermaLink="false"><?php echo $post['post_permalink']; ?></guid>
-                <description><![CDATA[<?php echo apply_filters('the_excerpt_rss', wp_trim_excerpt($post['post_content'])); ?>]]></description>
-                <content:encoded><![CDATA[<?php echo apply_filters('the_content_feed', $post['post_content'], 'rss2'); ?>]]></content:encoded>
-				<wfw:commentRss><?php echo $post['post_permalink'] . 'feed/'; ?></wfw:commentRss>
-			</item>
-			<?php
-		}
-	}
-	//--------------------------------------------------------------------//
-	?>
+	<lastBuildDate><?php echo mysql2date('D, d M Y H:i:s +0000', network_get_lastpostmodified('GMT'), false); ?></lastBuildDate>
+	<language><?php bloginfo_rss( 'language' ); ?></language>
+	<sy:updatePeriod><?php echo apply_filters( 'rss_update_period', 'hourly' ); ?></sy:updatePeriod>
+	<sy:updateFrequency><?php echo apply_filters( 'rss_update_frequency', '1' ); ?></sy:updateFrequency>
+	<?php do_action('rss2_head'); ?>
+	<?php while( network_have_posts()) : network_the_post(); ?>
+	<item>
+		<title><?php network_the_title_rss(); ?></title>
+		<link><?php network_the_permalink_rss(); ?></link>
+		<comments><?php network_comments_link_feed(); ?></comments>
+		<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', network_get_post_time('Y-m-d H:i:s', true), false); ?></pubDate>
+		<dc:creator><?php network_the_author(); ?></dc:creator>
+		<?php network_the_category_rss('rss2'); ?>
+
+		<guid isPermaLink="false"><?php network_the_guid(); ?></guid>
+<?php if (get_option('rss_use_excerpt')) { ?>
+		<description><![CDATA[<?php network_the_excerpt_rss(); ?>]]></description>
+<?php } else { ?>
+		<description><![CDATA[<?php network_the_excerpt_rss() ?>]]></description>
+	<?php if ( strlen( $network_post->post_content ) > 0 ) { ?>
+		<content:encoded><![CDATA[<?php network_the_content_feed('rss2'); ?>]]></content:encoded>
+	<?php } else { ?>
+		<content:encoded><![CDATA[<?php network_the_excerpt_rss(); ?>]]></content:encoded>
+	<?php } ?>
+<?php } ?>
+		<wfw:commentRss><?php echo esc_url( network_get_post_comments_feed_link(null, 'rss2') ); ?></wfw:commentRss>
+		<slash:comments><?php echo network_get_comments_number(); ?></slash:comments>
+<?php network_rss_enclosure(); ?>
+	<?php do_action('network_rss2_item'); ?>
+	</item>
+	<?php endwhile; ?>
 </channel>
 </rss>
-<?php
-//------------------------------------------------------------------------//
-?>
